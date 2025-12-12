0828d8239e82e527383bea48d7c9e926c3df0161
zremboldt <zac.remboldt@joinroot.com>
2025-12-11 22:43:07 -0600

    Consolodate into a reusable RouteHandler type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,4 @@
-import type { OpenAPIHono } from "@hono/zod-openapi";
+import type { OpenAPIHono, RouteConfig, RouteHandler } from "@hono/zod-openapi";
 import type { Logger } from "pino";
 
 export interface AppBindings {
@@ -9,3 +9,5 @@ export interface AppBindings {
 }
 
 export type AppOpenAPI = OpenAPIHono<AppBindings>;
+
+export type AppRouteHandler<R extends RouteConfig> = RouteHandler<R, AppBindings>;
