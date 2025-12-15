a6a6cd013cfede006ec5484b80a0d855fa922f2f
zremboldt <zac.remboldt@joinroot.com>
2025-12-14 23:12:28 -0600

    Improve db query

--- src/routes/users/users.handlers.ts
+++ src/routes/users/users.handlers.ts
@@ -2,12 +2,12 @@ import { drizzle } from "drizzle-orm/d1";
 
 import type { AppRouteHandler } from "@/lib/types";
 
+import * as schema from "@/db";
+
 import type { ListRoute } from "./users.routes";
 
-import { users } from "./users.schema";
-
 export const list: AppRouteHandler<ListRoute> = async (c) => {
-  const db = drizzle(c.env.DB);
-  const result = await db.select().from(users).all();
-  return c.json(result);
+  const db = drizzle(c.env.DB, { schema });
+  const users = await db.query.users.findMany();
+  return c.json(users);
 };
