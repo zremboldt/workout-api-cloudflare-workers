fe3d64385b084fb374a7207ced45bff939177d08
zremboldt <zac.remboldt@joinroot.com>
2025-12-16 21:36:44 -0600

    Add types to tests

--- src/routes/users/users.test.ts
+++ src/routes/users/users.test.ts
@@ -1,5 +1,12 @@
-import { SELF } from "cloudflare:test";
+import { env } from "cloudflare:test";
+import { testClient } from "hono/testing";
 import { beforeEach, describe, expect, expectTypeOf, it } from "vitest";
+
+import { createApp } from "@/lib/create-app";
+
+import { users } from "./users.index";
+
+const client = testClient(createApp().route("/", users), env);
 
 describe("users", () => {
   const testUser = {
@@ -10,40 +17,38 @@ describe("users", () => {
 
   beforeEach(async () => {
     // Test DB is ephemeral, so we seed a user for each test
-    await SELF.fetch("https://test/users", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(testUser),
+    await client.users.$post({
+      json: testUser,
     });
   });
 
   it("lists all users", async () => {
-    const response = await SELF.fetch("https://test/users");
+    const response = await client.users.$get();
     expect(response.status).toBe(200);
 
-    const users = await response.json();
-    expectTypeOf(users).toBeArray();
-    expect(users).toHaveLength(1);
-    expect(users[0]).toMatchObject(testUser);
+    if (response.status === 200) {
+      const json = await response.json();
+      expectTypeOf(json).toBeArray();
+      expect(json).toHaveLength(1);
+      expect(json[0]).toMatchObject(testUser);
+    }
   });
 
   it("creates a user", async () => {
-    const response = await SELF.fetch("https://test/users", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        firstName: "Jane",
-        lastName: "Smith",
-        email: "jane.smith@example.com",
-      }),
-    });
-
-    expect(response.status).toBe(200);
-    const user = await response.json();
-    expect(user).toMatchObject({
+    const newUser = {
       firstName: "Jane",
       lastName: "Smith",
       email: "jane.smith@example.com",
+    };
+
+    const response = await client.users.$post({
+      json: newUser,
     });
+
+    expect(response.status).toBe(200);
+    if (response.status === 200) {
+      const json = await response.json();
+      expect(json).toMatchObject(newUser);
+    }
   });
 });
