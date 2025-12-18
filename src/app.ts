18515fba64812f1ae0bf3384a251443842a3ee59
zremboldt <zac.remboldt@joinroot.com>
2025-12-17 20:36:08 -0600

    Export types for entire API

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,16 @@ const app = createApp();
 const routes = [
   index,
   users,
-];
+] as const; // Lets TypeScript know that this array is not going to change at runtime, which allows us to get the type out of it.
 
 configureOpenAPI(app); // This sets up our documentation endpoints
 
 routes.forEach((route) => {
   app.route("/", route);
 });
+
+// This will give us the type of everything at routes[i], which is the type of each route module.
+// This is useful because in a monorepo scenario, we can use this on our client to get types for all routes.
+export type AppType = typeof routes[number];
 
 export default app;
